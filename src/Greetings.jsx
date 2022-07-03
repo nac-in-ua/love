feea122b8962ce658e387472ecacbab32e92732d
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-03 16:07:35 +0300

    minor chnages

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,8 +1,11 @@
 function Greetings() {
   return (
-    <h1 className="my-10 text-center font-roboto text-7xl font-light text-stone-200">
-      I love you so much!
-    </h1>
+    <>
+      <h1 className="my-10 text-center font-roboto text-7xl font-light text-stone-200">
+        I love you so much!
+      </h1>
+      <div className="font-roboto text-stone-200">really!</div>
+    </>
   )
 }
 
